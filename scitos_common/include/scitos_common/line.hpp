9b9762e39e91bb839a0a16f59541cd3ad2c92a99
makits <makits@ttu.ee>
2022-03-02 21:15:33 +0000

    Removing slope_ and yIntersept_ from line class

--- scitos_common/include/scitos_common/line.hpp
+++ scitos_common/include/scitos_common/line.hpp
@@ -12,8 +12,6 @@ public:
     void setEnd(const std::vector<Vec2<float>>& e);
     float getConfidance();
     void setConfidance(float conf);
-    float slope();
-    float yIntersept();
     std::vector<Vec2<float>> start();
     std::vector<Vec2<float>> end();
     bool isParallel(Line);
@@ -22,65 +20,62 @@ public:
     bool merge(Line);
     float twoPointDistance(const std::vector<Vec2<float>>& s, const std::vector<Vec2<float>>& e);
     Vec2<T> newPoint(const std::vector<Vec2<float>>& s);
+    float slope();
+    float yIntersept();
 private:
     std::vector<Vec2<float>> start_;
     std::vector<Vec2<float>> end_;
     float confidance_ = 0;
-    float slope_;
-    float yIntersept_;
-    void findFunction();
-
 };
 
 
 Line::Line(const std::vector<Vec2<float>>& s, const std::vector<Vec2<float>>& e)
 {
-    start_ = s;  end_ = e; Line::findFunction();
+    start_ = s;  end_ = e;
 }
 
-void Line::setStart(const std::vector<Vec2<float>>& s) { start_ = s; findFunction(); }
+void Line::setStart(const std::vector<Vec2<float>>& s) { start_ = s; }
 
-void Line::setEnd(const std::vector<Vec2<float>>& e) { end_ = e; findFunction(); }
+void Line::setEnd(const std::vector<Vec2<float>>& e) { end_ = e; }
 
 float Line::getConfidance() { return confidance_; }
 
 void Line::setConfidance(float conf) { confidance_ = conf; }
 
-float Line::slope() { return slope_; }
+float Line::slope() { 
+    return (end_.y - start_.y) / (end_.x - start_.x);
+}
 
-float Line::yIntersept() { return yIntersept_; }
+float Line::yIntersept() { 
+    return start_.y - slope() * start_.x;
+}
 
 std::vector<Vec2<float>> Line::start() { return start_; }
 
 std::vector<Vec2<float>> Line::end() { return end_; }
 
-void Line::findFunction() {
-
-    slope_ = (end_.y - start_.y) / (end_.x - start_.x);
-    yIntersept_ = start_.y - slope_ * start_.x;
-}
 
 bool Line::isParallel(Line line) { // if lines are parallel and close
     float parameter = 5;
-    if (yIntersept_-line.yIntersept() <= parameter) // are line interseptor values close 
+    if (yIntersept() - line.yIntersept() <= parameter) // are line interseptor values close 
     {
-        if (slope_ == 0 && (line.slope() == 0))
+        if (slope() == 0 && (line.slope() == 0))
         {
             return true;
         }
-        else if (slope_ == 0) {
+        else if (slope() == 0) {
             if (abs(line.slope()) == 0.05) {
                 return true;
             }
             return false;
         }
         else if (line.slope() == 0) {
-            if (abs(slope_) == 0.05) {
+            if (abs(slope()) == 0.05) {
                 return true;
             }
             return false;
         }
-        else if ((line.slope() / slope_) > 0.95 && (line.slope() / slope_) < 1.05) {
+        else if ((line.slope() / slope()) > 0.95 && (line.slope() / slope()) < 1.05) {
             return true;
         }
     }
@@ -132,7 +127,7 @@ bool Line::merge(Line line) { // first to merge all lines and then use connect t
 bool Line::closeToLine(const std::vector<Vec2<float>>& s) {
 
     float parameter = 5; // parameter to regulate how close is close :D
-    float d1 = abs(slope_ * s.x + s.y + yIntersept_) / sqrt(pow(slope_, 2) + 1); // point distance to line
+    float d1 = abs(slope() * s.x + s.y + yIntersept()) / sqrt(pow(slope(), 2) + 1); // point distance to line
     float d2 = twoPointDistance(start_, s); // start distance to point
     float d3 = twoPointDistance(s, end_); // end distance to point
     float d4 = twoPointDistance(start_, end_); // line lenght
@@ -148,7 +143,6 @@ bool Line::closeToLine(const std::vector<Vec2<float>>& s) {
 
 float Line::twoPointDistance(const std::vector<Vec2<float>>& s, const std::vector<Vec2<float>>& e) {
 
-    float d = sqrt((s.x + e.x) ^ 2 + (s.y + e.y) ^ 2);
-    return d;
+    return sqrt((s.x + e.x) ^ 2 + (s.y + e.y) ^ 2);
 
 }
