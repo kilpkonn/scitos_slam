8bd52010bc93d2b0c956f2c022721bacb466195d
Tavo Annus <tavo.annus@gmail.com>
2022-03-19 22:20:27 +0200

    Add V matrix to EKF

--- scitos_common/include/scitos_common/ekf.hpp
+++ scitos_common/include/scitos_common/ekf.hpp
@@ -26,20 +26,23 @@ template <typename T> class EKF {
   void step(Vector3f m_old, MatrixXd sigma_old, Vector2f u, float dt,
             std::vector<map::Line<T>> z, map::Map<T> map) {
     // Motion update
-    Vector3f m{m_old(0) + u(0) * dt * std::cos(m_old(2) + u(2) * dt / 2),
-               m_old(1) + u(0) * dt * std::sin(m_old(2) + u(2) * dt / 2),
+    Vector3f m{m_old(0) + u(0) * dt * std::cos(m_old(2) + u(1) * dt / 2.f),
+               m_old(1) + u(0) * dt * std::sin(m_old(2) + u(1) * dt / 2.f),
                m_old(2) + u(1) * dt};
 
     Matrix2f M{{a1 * u(0) * u(0) + a2 * u(1) * u(1), 0},
                {0, a3 * u(0) * u(0) + a4 * u(1) * u(1)}};
 
-    float rot1, trans, rot2; // TODO: actual values, but from where?
-    // HACK: Pretty sure this matrix is incorrect, need to take Jacobian on
-    // paper to recheck...
-    Matrix3f G{{1.f, 0.f, -trans * std::sin(m_old(2) + rot1)},
-               {0.f, 1.f, trans * std::cos(m_old(2) + rot1)},
+    Matrix3f G{{1.f, 0.f, -u(0) * dt * std::sin(m_old(2) + u(1) * dt / 2.f)},
+               {0.f, 1.f, u(0) * dt * std::cos(m_old(2) + u(1) * dt / 2.f)},
                {0.f, 0.f, 1.f}};
-    Matrix3f V{{}, {}, {}};
+
+    // TODO: This should be 3 row 2 col matrix
+    Matrix3f V{{dt * std::cos(m_old(2) + u(1) * dt / 2.f),
+                -0.5f * dt * dt * std::sin(m_old(2) + u(1) * dt / 2.f)},
+               {dt * std::sin(m_old(2) + u(1) * dt / 2.f),
+                0.5f * dt * dt * std::cos(m_old(2) + u(1) * dt / 2.f)},
+               {0.f, dt}};
 
     Matrix3f sigma = G * sigma_old * G.transpose() + V * M * V.transpose();
 
