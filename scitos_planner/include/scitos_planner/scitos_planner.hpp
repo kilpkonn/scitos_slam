59c507dc562e84f4ba428a25113889d89548df5a
Tavo Annus <tavo.annus@gmail.com>
2022-04-16 13:07:33 +0300

    Listen for map in scitos planner

--- scitos_planner/include/scitos_planner/scitos_planner.hpp
+++ scitos_planner/include/scitos_planner/scitos_planner.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 #include <ros/ros.h>
+
+#include "scitos_common/LineArray.h"
+
+#include "scitos_common/map/map.hpp"
 
 class Planner {
 public:
@@ -11,4 +16,10 @@ public:
 
 private:
   ros::NodeHandle nh_;
+
+  ros::Subscriber mapSub_;
+
+  scitos_common::map::Map<float> map_;
+
+  void mapCallback(scitos_common::LineArray msg);
 };
--- scitos_planner/src/scitos_planner.cpp
+++ scitos_planner/src/scitos_planner.cpp
@@ -1,8 +1,25 @@
 
 #include <ros/package.h>
+#include <utility>
+#include <vector>
 
+#include "scitos_common/LineArray.h"
+#include "scitos_common/map/line.hpp"
 #include "scitos_planner/scitos_planner.hpp"
 
-Planner::Planner(ros::NodeHandle nh) : nh_{nh} {}
+Planner::Planner(ros::NodeHandle nh) : nh_{nh} {
+    mapSub_ =
+      nh_.subscribe("/map", 1, &Planner::mapCallback, this);
+
+}
 
 void Planner::step(const ros::TimerEvent &event) {}
+
+void Planner::mapCallback(scitos_common::LineArray msg) {
+  std::vector<scitos_common::map::Line<float>> lines;
+  lines.reserve(msg.lines.size());
+  for (const auto l : msg.lines) {
+      lines.push_back({{l.x1, l.y1}, {l.x2, l.y2}});
+  }
+  map_.loadFromLines(lines);
+}
